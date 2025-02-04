90feab040bc9d13a8eb085933d81338fbb1fd02d
chetan71 <91386497+chetan71@users.noreply.github.com>
2025-02-04 23:52:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+
+  // Getter for age (optional for this task, but useful for validation or display)
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);  // Call the parent class constructor
+  }
+
+  // Method for studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);  // Call the parent class constructor
+  }
+
+  // Method for teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
